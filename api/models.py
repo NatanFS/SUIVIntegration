2b5cc8c5f7dfeccc8363736dc56e31aaa9a851b3
Natan Felipe de Freitas Souza <natanlipf@gmail.com>
2023-03-14 15:43:13 -0300

    Adicionados modelos. Salvando dados no banco, registrando chamada de endpoint.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+from datetime import datetime
+
+class FipeData(models.Model):
+    year = models.IntegerField()
+    fipe_id = models.IntegerField()
+    maker_description = models.CharField(max_length=255)
+    model_description = models.CharField(max_length=255)
+    version_description = models.CharField(max_length=255)
+    fuel = models.CharField(max_length=255)
+    current_value = models.FloatField()
+
+class PriceHistory(models.Model):
+    fipe_data = models.ForeignKey(FipeData, on_delete=models.CASCADE, related_name='price_history')
+    month_update = models.IntegerField()
+    year_update = models.IntegerField()
+    value = models.FloatField()
+    is_prediction = models.BooleanField()
+
+class SuivData(models.Model):
+    fipe_id = models.IntegerField()
+    version_id = models.IntegerField()
+    version_description = models.CharField(max_length=255)
+    model_id = models.IntegerField()
+    model_description = models.CharField(max_length=255)
+    maker_id = models.IntegerField()
+    maker_description = models.CharField(max_length=255)
+
+class Vehicle(models.Model):
+    maker = models.CharField(max_length=255)
+    maker_id = models.IntegerField()
+    fipe_id = models.IntegerField()
+    description = models.CharField(max_length=255)
+    plate = models.CharField(max_length=255)
+    year_model = models.IntegerField()
+    year_fab = models.IntegerField()
+    fuel = models.CharField(max_length=255)
+    chassis = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    species = models.CharField(max_length=255)
+    bodywork = models.CharField(max_length=255)
+    power = models.IntegerField()
+    is_national = models.BooleanField()
+    axis_count = models.IntegerField()
+    total_gross_weight = models.FloatField()
+    maximum_traction_capacity = models.FloatField()
+    cc = models.IntegerField()
+    seat_count = models.IntegerField()
+    load_capacity = models.FloatField()
+    gear_box_number = models.CharField(max_length=255)
+    back_axis_count = models.CharField(max_length=255)
+    aux_axis_count = models.CharField(max_length=255)
+    engine_number = models.CharField(max_length=255)
+
+class SUIVRequest(models.Model):
+    endpoint = models.CharField(max_length=255)
+    date = models.DateTimeField(default=datetime.now)
+
--- api/views.py
+++ api/views.py
@@ -1,21 +1,165 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 import requests
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from decouple import config
+from django.core import serializers
+import json
+
+from api.models import FipeData, PriceHistory, SUIVRequest, SuivData, Vehicle
 # Create your views here.
 class InformacoesVeiculoView(APIView):
     def get(self, request, *args, **kwargs):
-        placa = request.GET.get('placa')
-        if not placa:
-            return Response({'error': 'Placa não informada'}, status=400)
-        placa = placa.replace('-', '')
+        plate = request.GET.get('placa')
+        if not plate:
+            return Response({'error': 'Plate not informed'}, status=400)
 
-        api_key = config('SUIV_API_KEY')
-        print(api_key)
-        # url = f'https://api.example.com/vehicle-info?plate={placa}'
-        # response = requests.get(url)
-        # if not response.ok:
-        #     return Response({'error': 'Falha ao recuperar dados'}, status=500)
+        # Remove hífen da placa
+        plate = plate.replace('-', '')
 
-        return Response({"message": "success"})
+        # Procura dados no banco
+        suiv_data_objects = None
+        fipe_data_objects = None
+        vehicle = Vehicle.objects.filter(plate=plate)
+        if vehicle:
+            vehicle = vehicle[0]
+            suiv_data_objects = SuivData.objects.filter(fipe_id=vehicle.fipe_id)
+            fipe_data_objects = FipeData.objects.filter(fipe_id=vehicle.fipe_id)
+        else:
+            # Recupera dados da SUIV, caso não haja no banco de dados
+            api_key = config('SUIV_API_KEY')
+            endpoint = 'api/v3/VehicleInfo/byplate'
+            url = f'https://api.suiv.com.br/{endpoint}?plate={plate}&key={api_key}'
+            response = requests.get(url)
+
+            # Registra chamada à SUIV
+            register_suiv_request(endpoint)
+
+            if not response.ok:
+                return Response({'error': 'Fail retrieving data'}, status=500)
+
+            # Salva dados no banco
+            if response.status_code == 200:
+                data = response.json()
+                vehicle, suiv_data_objects, fipe_data_objects = save_suiv_data(data)
+
+        json_data = generate_vehicle_info_json(vehicle, suiv_data_objects, fipe_data_objects)
+
+        return JsonResponse(json_data)
+
+def generate_vehicle_info_json(vehicle, fipe_data_objects, suiv_data_objects):
+    # Serializa dados em JSON
+    vehicle_data = serializers.serialize('json', [vehicle])
+    fipe_data_collection = serializers.serialize('json', fipe_data_objects, \
+    use_natural_foreign_keys=True, relations={'price_history': {'use_natural_primary_keys': True}})
+    suiv_data_collection = serializers.serialize('json', suiv_data_objects)
+
+    # Convert JSON strings into Python dictionaries
+    vehicle_data = json.loads(vehicle_data)[0]['fields']
+    fipe_data_collection = json.loads(fipe_data_collection)
+    suiv_data_collection = json.loads(suiv_data_collection)
+
+    # Create the final JSON object
+    data = {
+        **vehicle_data,
+        "fipeDataCollection": fipe_data_collection,
+        "suivDataCollection": suiv_data_collection
+    }
+
+    # 
+    json_data = json.dumps(data)
+    return json_data
+
+def register_suiv_request(endpoint):
+    SUIVRequest.objects.create(endpoint=endpoint)
+
+def save_suiv_data(data):
+    fipe_data_collection = data.get('fipeDataCollection', [])
+    suiv_data_collection = data.get('suivDataCollection', [])
+    vehicle_data = {k: v for k, v in data.items() if k not in ['fipeDataCollection', 'suivDataCollection']}
+
+    # Intancia objetos FipeData 
+    fipe_data_objects = save_fipe_data_objects(fipe_data_collection)
+
+    # Intancia objetos PriceHistory 
+    save_price_history_data_objects(fipe_data_collection)
+
+    # Intancia objetos SuivData
+    suiv_data_objects = save_suiv_data(suiv_data_collection)
+
+    # Instancia veículo
+    vehicle = save_vehicle_data_object(vehicle_data)
+
+    return (vehicle, suiv_data_objects, fipe_data_objects)
+
+def save_fipe_data_objects(fipe_data_collection):
+    fipe_data_objects = []
+    for fipe_data in fipe_data_collection:
+        fd_kwargs = {
+            'year': fipe_data['year'],
+            'fipe_id': fipe_data['fipeId'],
+            'maker_description': fipe_data['makerDescription'],
+            'model_description': fipe_data['modelDescription'],
+            'version_description': fipe_data['versionDescription'],
+            'fuel': fipe_data['fuel'],
+            'current_value': fipe_data['currentValue'],
+        }
+        fipe_data_objects.append(FipeData(**fd_kwargs))
+    return FipeData.objects.bulk_create(fipe_data_objects)
+
+def save_price_history_data_objects(fipe_data_collection):
+    price_history_objects = []
+    for fipe_data in fipe_data_collection:
+        fipe_data_obj = FipeData.objects.get(fipe_id=fipe_data['fipeId'])
+        for price_history in fipe_data['priceHistory']:
+            ph_kwargs = {
+                'month_update': price_history['monthUpdate'],
+                'year_update': price_history['yearUpdate'],
+                'value': price_history['value'],
+                'is_prediction': price_history['isPrediction'],
+                'fipe_data': fipe_data_obj,
+            }
+            price_history_objects.append(PriceHistory(**ph_kwargs))
+    return PriceHistory.objects.bulk_create(price_history_objects)
+
+def save_suiv_data_objects(suiv_data_collection):
+    suiv_data_objects = []
+    for suiv_data in suiv_data_collection:
+        sd_kwargs = {
+            'fipe_id': suiv_data['fipeId'],
+            'version_id': suiv_data['versionId'],
+            'version_description': suiv_data['versionDescription'],
+            'model_id': suiv_data['modelId'],
+            'model_description': suiv_data['modelDescription'],
+            'maker_id': suiv_data['makerId'],
+            'maker_description': suiv_data['makerDescription'],
+        }
+        suiv_data_objects.append(SuivData(**sd_kwargs))
+    return SuivData.objects.bulk_create(suiv_data_objects)
+
+
+def save_vehicle_data_object(vehicle_data):
+    vehicle_kwargs = {
+        'plate': vehicle_data['plate'],
+        'year_model': vehicle_data['yearModel'],
+        'year_fab': vehicle_data['yearFab'],
+        'fuel': vehicle_data['fuel'],
+        'chassis': vehicle_data['chassis'],
+        'type': vehicle_data['type'],
+        'species': vehicle_data['species'],
+        'bodywork': vehicle_data['bodywork'],
+        'power': vehicle_data['power'],
+        'is_national': vehicle_data['isNational'],
+        'axis_count': vehicle_data['axisCount'],
+        'total_gross_weight': vehicle_data['totalGrossWeight'],
+        'maximum_traction_capacity': vehicle_data['maximumTractionCapacity'],
+        'gear_box_number': vehicle_data['maximumTractionCapacity'],
+        'back_axis_count': vehicle_data['backAxisCount'],
+        'aux_axis_count': vehicle_data['auxAxisCount'],
+        'engine_number': vehicle_data['engineNumber']
+    }
+
+    vehicle = Vehicle(**vehicle_kwargs)
+    vehicle.save()
+    return vehicle
